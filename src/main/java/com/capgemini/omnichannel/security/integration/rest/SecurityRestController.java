1a04ab5a68dd3231c3f2af420d5a9c43e67ec0df
Carlos Verdes <cverdes@gmail.com>
2015-07-03 11:55:39 +0100

    change rest endpoint from token to tokens

--- src/main/java/com/capgemini/omnichannel/security/integration/rest/SecurityRestController.java
+++ src/main/java/com/capgemini/omnichannel/security/integration/rest/SecurityRestController.java
@@ -16,8 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("token")
-@RequestMapping("/token")
+@RestController("tokens")
+@RequestMapping("/tokens")
 
 /**
  * Based on JSON Token Authentication
