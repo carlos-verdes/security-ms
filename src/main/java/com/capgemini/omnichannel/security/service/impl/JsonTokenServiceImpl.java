8eefcc462a6a1bc5badfc6a5d04030d1e09fdca1
Carlos Verdes <cverdes@gmail.com>
2015-07-14 12:45:45 +0200

    change to rsa

--- src/main/java/com/capgemini/omnichannel/security/service/impl/JsonTokenServiceImpl.java
+++ src/main/java/com/capgemini/omnichannel/security/service/impl/JsonTokenServiceImpl.java
@@ -1,10 +1,17 @@
 package com.capgemini.omnichannel.security.service.impl;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.crypto.MacProvider;
+import io.jsonwebtoken.impl.crypto.RsaProvider;
+import io.jsonwebtoken.impl.crypto.RsaSignatureValidator;
 
+import java.security.KeyPair;
 import java.security.Principal;
+import java.security.interfaces.RSAKey;
 import java.util.List;
 
 import javax.crypto.SecretKey;
@@ -12,7 +19,6 @@ import javax.crypto.SecretKey;
 import com.capgemini.omnichannel.security.service.TokenService;
 
 public class JsonTokenServiceImpl implements TokenService {
-
 
 	@Override
 	public String getResourceId(String resource) {
@@ -44,20 +50,32 @@ public class JsonTokenServiceImpl implements TokenService {
 		return null;
 	}
 
-
-	
 	/**
 	 * Generates a token from the string received
+	 * 
 	 * @param user
 	 * @return
 	 */
-	private String generateToken(String user) {
+	private static String generateToken(String user) {
 		String token;
-		
-		SecretKey key = MacProvider.generateKey();
-		token = Jwts.builder().setSubject(user).signWith(SignatureAlgorithm.HS512, key).compact();
-		
+		KeyPair keyPair = RsaProvider.generateKeyPair();
+		System.out.println(String.format("private key \n %s", keyPair.getPrivate().toString()));
+		System.out.println(String.format("public key \n %s", keyPair.getPrivate().toString()));
+
+		token = Jwts.builder().setSubject(user).signWith(SignatureAlgorithm.RS256, keyPair.getPrivate()).compact();
+
+		Jws<Claims> parsedtoken = Jwts.parser().setSigningKey(keyPair.getPublic()).parseClaimsJws(token);
+
+		System.out.println(String.format("subject: %s", parsedtoken.getBody().getSubject()));
+
 		return token;
 	}
 
+	public static void main(String[] args) {
+		
+		String user="cinfantes";
+		
+		generateToken(user);
+	}
+	
 }
