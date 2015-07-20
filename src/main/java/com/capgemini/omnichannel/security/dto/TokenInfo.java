4480801ee15d1aba4ad0efe5db475cb091ecf80a
Carlos Verdes <cverdes@gmail.com>
2015-07-20 13:55:47 +0100

    change hash number

--- src/main/java/com/capgemini/omnichannel/security/dto/TokenInfo.java
+++ src/main/java/com/capgemini/omnichannel/security/dto/TokenInfo.java
@@ -32,7 +32,7 @@ public class TokenInfo implements Serializable{
 
 	@Override
 	public int hashCode() {
-		final int prime = 31;
+		final int prime = 1613;
 		int result = 1;
 		result = prime * result + ((subject == null) ? 0 : subject.hashCode());
 		return result;
